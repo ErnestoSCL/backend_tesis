e774b7162550f4dd0cee58faeb13826ad93acf8c
ErnestoSCL <ernestosaniel123@gmail.com>
2025-05-09 17:25:12 -0500

    Database_url agregada

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 from typing import Union
-
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+# Cargar el archivo .env
+load_dotenv()
+
+# Obtener la variable de entorno
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL is None:
+    raise RuntimeError("DATABASE_URL no está definida en el archivo .env")
 
 app = FastAPI()
 
